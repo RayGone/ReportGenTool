d2a343b34fd91e349e9a2324e6a6e6abbceb8ed9
raygone <gregatom99@gmail.com>
2022-05-17 20:27:34 +0545

    changed UI color theme, moved CSS to separate file, favicon added

--- index.php
+++ index.php
@@ -3,63 +3,26 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tool</title>
+    <link rel='icon' type='image/png' href='c_logo.png'>
     <link rel="stylesheet" href="/bootstrap4/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap5/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #eee;
-        }
-
-        .jumbotron {
-            padding: 20px;
-            background-color: #dfdfdf;
-        }
-
-        .row {
-            padding-bottom: 20px;
-        }
-
-        .modal {
-            position: fixed;
-            height: 100vh;
-            width: 100vw;
-            background-color: #333333aa;
-            display: block;
-        }
-
-        .td{
-            text-shadow: 0 0 5px #fff;
-        }
-
-        .table-responsive{
-            background-color: #eeeeee88;
-            box-shadow: 2px 2px 5px #333, -2px -2px 5px #ccc;
-            padding: 5px;
-        }
-
-        .close-modal{
-            padding:10px;
-            margin-right: 30px;
-            float:right;font-size:2em;color:red;cursor:pointer;
-            text-shadow: 2px 2px 2px #fff, -1px -1px 2px #888;
-        }
-
-        .sp2{
-            animation-delay: 150ms;
-        }
-
-        .sp3{
-            animation-delay: 300ms;
-        }
-    </style>
+    <link rel='stylesheet' href='custom.css'>
 </head>
 
 <body>
-    <div class='container container-fluid'>
+    <div class='container-fluid'>
         <div class='row'>
-            <div class='col-md-12'>
+            <div class='col-md-10 offset-md-1'>
                 <div class='jumbotron'>
-                    <p class='title text-center display-6'>Generate Report From The CSV files</p>
+                    <br>
+                    <div class="row">
+                        <div class='col-md-2'><img src='c_logo.png' style='height:50px;width:50px;'></div>
+                        <div class='col-md-8'>
+                            <p class='title text-center display-6'>Covia's Report Generation Tool</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -70,7 +33,7 @@
                 <form>
                     <br>
                     <input webkitdirectory title="Select File Path" multiple id='files' onchange="fileSelectionObserver(this)" class='form-control' type='file' name='files' accept="text/csv">
-                    
+
                 </form>
             </div>
         </div>
@@ -79,11 +42,21 @@
             <div class='col-md-8 offset-2 d-none machine'>
                 <div class='row'>
                     <div class='display-6'>Generate Report</div>
-                    <div class='col-sm-4'><label for='from'>From: </label>&nbsp;&nbsp;&nbsp;&nbsp;<input class='form-control' type='date' name='from' date></div>
-                    <div class='offset-4 col-sm-4'><label for='from'>To: </label>&nbsp;&nbsp;&nbsp;&nbsp;<input class='form-control' type='date' name='to' date></div>
+                    <div class='col-sm-3'><label for='from'>From: </label>&nbsp;&nbsp;&nbsp;&nbsp;<input class='form-control' type='date' name='from' date></div>
+                    <div class='offset-1 col-sm-3'><label for='from'>To: </label>&nbsp;&nbsp;&nbsp;&nbsp;<input class='form-control' type='date' name='to' date></div>
                 </div><br>
-                <label for='filter'>Take average of: <span id='agg-val'>5</span>min</label>
-                <input name='filter' type='range' min=1 max=120 style='width:100%;height:2em' oninput="document.getElementById('agg-val').innerHTML = this.value" value=5>
+                <label for='filter'>Take average of: <span id='agg-val'>5min</span></label>
+                <div class="wrap">
+                    <input name='filter' type='range' min=1 max=7200 style='width:100%;height:2em' oninput="updateDateRangeLabel(this)" value=300>
+                </div>
+                <script>
+                    function updateDateRangeLabel(range) {
+                        let v = parseInt(range.value);
+                        let m = parseInt(v / 60);
+                        let s = parseInt(((v / 60) - m) * 10);
+                        document.getElementById('agg-val').innerHTML = `${m}min ${s}s`;
+                    }
+                </script>
                 <nav class="alert alert-primary">
                     <div class="" href="#">&nbsp;&nbsp;Select Machine: &nbsp;&nbsp;&nbsp;<span id='sel-mac' class='dispaly-4 text-center'></span> </div>
                 </nav>
@@ -101,8 +74,8 @@
         <div class="row loader">
             <div class="col-md-4 offset-4 text-center">
                 <div class="spinner-grow spinner-grow-xl text-info" style="width: 5rem; height: 5rem;" role="status"></div>
-                <div class="spinner-grow spinner-grow-xl text-info sp2" style="width: 5rem; height: 5rem;" role="status"></div>
-                <div class="spinner-grow spinner-grow-xl text-info sp3" style="width: 5rem; height: 5rem;" role="status"></div>
+                &nbsp;&nbsp;<div class="spinner-grow spinner-grow-xl text-info sp2" style="width: 5rem; height: 5rem;" role="status"></div>
+                &nbsp;&nbsp;<div class="spinner-grow spinner-grow-xl text-info sp3" style="width: 5rem; height: 5rem;" role="status"></div>
             </div>
             <div class='col-md-12 text-center'>
                 <span class='display-4 td'>Processing....</span>
@@ -110,7 +83,7 @@
         </div>
         <div class='row cs d-none'>
             <div class='col-md-4 offset-4 text-center'>
-                <div class="alert alert-success display-6">
+                <div class="alert alert-primary display-6">
                     Processing Complete.
                 </div>
             </div>
